ac68105002aabef29fbf8dae42cf572de9c792d5
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2022-10-06 17:22:17 +0100

    🐱‍👤Working on textbox UI

--- src/components/UI/textBox.tsx
+++ src/components/UI/textBox.tsx
@@ -1,29 +1,66 @@
 import "../../styles/designlab.css";
-import { useRef } from "react";
+import { useRef, useMemo, useState, useEffect } from "react";
 export default function textBox() {
-  const textarea = useRef();
-  const Hash = () => {
+  const [active, setactive] = useState(false);
+  const textBox: any = useRef();
+  let loadcounter = 0;
+  useEffect(() => {
+    loadcounter++;
+    if (loadcounter > 1) return CheckActiveState();
+    else;
+    return () => {};
+  });
+
+  const message = {
+    value: "Hello World!",
+  };
+  const textarea: any = useRef();
+  const spanElement: any = useRef();
+  const memoValue = useMemo(() => message, [message]);
+  const [detectFormError, setdetectFormError] = useState(false);
+
+  const HashButton = () => {
     const text_box = textarea.current;
     if (!text_box.value) {
-      console.log("text box is empty");
+      message.value =
+        "Hey Cutie💖 before you can make a post you would need to type something first :)";
+      setdetectFormError(true);
+      spanElement.current.textContent = memoValue.value;
     } else {
       console.log("text box is not empty");
+      message.value = "";
+      setdetectFormError(false);
+      spanElement.current.textContent = memoValue.value;
     }
   };
+  function CheckActiveState() {
+    const getActiveState = localStorage.getItem("active");
+
+    if (getActiveState === "true") {
+      setactive(true);
+      textBox.current.classList.add("active");
+    } else {
+      setactive(false);
+      textBox.current.classList.remove("active");
+    }
+  }
   return (
-    <div id="textBox">
+    <div className="textBox" ref={textBox}>
       <div id="box-cols">
-        <div id="bx-cl-left">
+        <div
+          id="bx-cl-left"
+          onClick={() => localStorage.setItem("active", "false")}
+        >
           <i className="bx bx-arrow-back"></i>
         </div>
         <div id="bx-cl-right">
-          <button onClick={Hash}>Hash</button>
+          <button onClick={HashButton}>Hash</button>
         </div>
       </div>
       <div className="hashtag-text-component">
         <textarea name="" id="" ref={textarea} cols={30} rows={10}></textarea>
-        <div className="error-component">
-          <span id="error">Hello from Error</span>
+        <div className={detectFormError ? "error-component" : null}>
+          <span id="error" ref={spanElement}></span>
         </div>
       </div>
     </div>
