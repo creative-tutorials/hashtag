8db20ec79cf668e9bf7cb09cf7ebccd99b178454
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2022-10-06 20:28:02 +0100

    🚧 Working on authentication page

---
+++ src/auth/Login.tsx
@@ -0,0 +1,5 @@
+function LoginPageComponent() {
+  /** @return {"Work in progress"} */
+  return <h1>login</h1>;
+}
+export default LoginPageComponent;
---
+++ src/auth/Signup.tsx
@@ -0,0 +1,5 @@
+function SignupPageComponent() {
+  /** @return {"Work in progress"} */
+  return <h1>signup</h1>;
+}
+export default SignupPageComponent;
