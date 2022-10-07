47b4f1e3f051236e1afa5892171b370291b2c025
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2022-10-07 19:40:24 +0100

    🚚 Added route login & signup page

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import reactLogo from "./assets/react.svg";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import HashtagHomePage from "./components/Home/Home";
-import Test from "./components/test/test";
-
+import LoginPageComponent from "./auth/Login";
+import SignupPageComponent from "./auth/Signup";
 function App() {
   return (
     <>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HashtagHomePage />} />
-        <Route path="/test" element={<Test />} />
+        <Route path="/login" element={<LoginPageComponent />} />
+        <Route path="/signup" element={<SignupPageComponent />} />
       </Routes>
     </BrowserRouter>
     
