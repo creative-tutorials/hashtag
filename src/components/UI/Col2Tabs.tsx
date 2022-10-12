3d0a7040947db2f01aa52cfca8d1ff5174bfa868
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2022-10-12 20:34:37 +0100

    📦 Compiled element components

--- src/components/UI/Col2Tabs.tsx
+++ src/components/UI/Col2Tabs.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Click } from "./textBox";
 import { VariableComponent } from "./textBox";
+import { HashPostComponent } from "../posts/posts";
 import { Item4 } from "../container/Item4";
 import { Item3 } from "../container/Item3";
 import { Item1 } from "../container/Item1";
 import { Item2 } from "../container/Item2";
+import { useEffect, useRef } from "react";
 export function Col2Tabs({ portrait, input, ReqFile, _box_item }: any) {
   VariableComponent;
+  
   return (
     <div className="col-2">
       <div className="col-2-header">
@@ -33,7 +36,6 @@ export function Col2Tabs({ portrait, input, ReqFile, _box_item }: any) {
         <div className="ipt-right-off-corner">
           <input
             type="file"
-            name=""
             id="file_upload"
             accept=".png,.jpg,.jpeg,.gif,,.jfif,.mp4"
             onChange={ReqFile}
@@ -56,57 +58,7 @@ export function Col2Tabs({ portrait, input, ReqFile, _box_item }: any) {
           <Item4 portrait={portrait} />
         </div>
       </div>
-      <div className="post_box">
-        <div className="nxd">
-          <div className="nxd-left">
-            <img src={portrait} alt="pfp" width={50} height={50} />
-            <div className="nxd-right">
-              <div className="name">
-                <span id="name">{"John Doe"}</span>
-              </div>
-              <div className="blue-check-mark">
-                {/* only for verified user's - coming soon */}
-                <span>check</span>
-              </div>
-              <div className="unique-name">
-                <span>{"@johndoe18"}</span>
-              </div>
-            </div>
-          </div>
-
-          <div className="nxd-outer-space">
-            <div className="dte">
-              <span id="date">20 Feb</span>
-            </div>
-            <span id="drpdwn-btn">
-              <i className="bx bx-dots-vertical-rounded"></i>
-            </span>
-          </div>
-        </div>
-        <div className="pst_body">
-          <div id="pst_content">
-            <span>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia
-              officiis dolorem soluta autem, laboriosam deserunt animi, tempore
-              explicabo, odio est eaque nesciunt nisi eveniet veniam officia.
-              Obcaecati, necessitatibus amet? Cum!
-            </span>
-          </div>
-          <div id="media-content">
-            <div id="includeImg">
-              <img src="" alt="imag" />
-            </div>
-            <div id="includeVid">
-              <video
-                src="/cj1.mp4"
-                autoPlay={true}
-                controls
-                muted={true}
-              ></video>
-            </div>
-          </div>
-        </div>
-      </div>
+      <HashPostComponent />
     </div>
   );
 }
