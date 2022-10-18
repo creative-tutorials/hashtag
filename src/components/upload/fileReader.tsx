d25584bc74cd5671b62f413e103ef7767d3029f7
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2022-10-18 21:38:22 +0100

    👾 Added the function that handles image posting

---
+++ src/components/upload/fileReader.tsx
@@ -0,0 +1,15 @@
+import { blobConverter } from "./requestedData";
+export async function ReadFileFromSystem(event: any) {
+  const FileObjectFromFileAPI = event.target.files[0];
+  const getFileNameFromFileAPI = event.target.files[0].name;
+  const getFileTypeFromFileAPI = event.target.files[0].type;
+  const getFileSizeFromFileAPI = event.target.files[0].size;
+  var reader = new FileReader();
+  reader.onload = async function () {
+    const Base64DataURLResult = reader.result;
+
+    blobConverter(Base64DataURLResult, getFileNameFromFileAPI, getFileTypeFromFileAPI, getFileSizeFromFileAPI, FileObjectFromFileAPI);
+    // DownloadFileLink(file);
+  };
+  reader.readAsDataURL(FileObjectFromFileAPI);
+}
