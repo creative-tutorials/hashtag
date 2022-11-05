4237236eb34c502ae3aed183013f6f44ec1e59c8
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2022-11-05 15:07:31 +0100

    🚚 renamed route path

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Loader from './components/loader';
+import Loader from "./components/loader";
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
@@ -14,18 +14,18 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Suspense fallback={<Loader     />}>
+        <Suspense fallback={<Loader />}>
           <Routes>
             <Route element={<ProtectedRoute />}>
               <Route path="/" element={<HashtagHomePage />} />
             </Route>
-            <Route path="/login" element={<LoginPageComponent />} />
-            <Route path="/signup" element={<SignupPageComponent />} />
+            <Route path="login" element={<LoginPageComponent />} />
+            <Route path="signup" element={<SignupPageComponent />} />
             <Route element={<ProtectedRoute />}>
-              <Route path="/profile" element={<ProfilePage />} />
+              <Route path="profile" element={<ProfilePage />} />
             </Route>
             <Route element={<ProtectedRoute />}>
-              <Route path="/trends" element={<TrendsPage />} />
+              <Route path="trends" element={<TrendsPage />} />
             </Route>
           </Routes>
         </Suspense>
