71ab5184baa1fc009a6d9d0090d35e0f436d6dfb
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2022-11-11 15:22:23 +0100

    ♻️ Refactoring code

--- src/components/posts/renderedImageElement.tsx
+++ src/components/posts/renderedImageElement.tsx
@@ -7,13 +7,22 @@ export function RenderedImagePost({ imagepostData, postui }: any) {
           <div className={postui.mediaContent}>
             <div className={postui.top}>
               <div className={postui.left}>
-                <img src={"/vite.svg"} alt="pfp" width={50} height={50} />
-                <span>{item.username}</span>
+                <img
+                  src={item.profile_image}
+                  alt="pfp"
+                  width={50}
+                  height={50}
+                  style={{ borderRadius: "50%", marginLeft: "10px" }}
+                />
+                <span style={{ marginLeft: "10px" }}>{item.username}</span>
               </div>
               <div className={postui.right_col}>
                 <div id={postui.date}>
                   <p>{item.created}</p>
                 </div>
+                <span id={postui.dropdwnbtn}>
+                  <i className="bx bx-dots-vertical-rounded"></i>
+                </span>
               </div>
             </div>
             <div id={postui.imageMediaContent}>
