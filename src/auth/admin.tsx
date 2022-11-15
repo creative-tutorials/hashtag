d692c847060df223a38bc744b36664fee7c91a1a
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2022-11-15 13:15:21 +0100

    🚧 Work in Progress

--- src/auth/admin.tsx
+++ src/auth/admin.tsx
@@ -1,5 +1,16 @@
 const AdminControl = () => {
-  return <></>;
+  return (
+    <div style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexDirection: 'column',
+      minHeight: '100vh'
+    }}>
+      <img src={"/Designer.gif"} alt="" />
+      <h1>This page is being worked on...</h1>
+    </div>
+  );
 };
 
 export default AdminControl;
